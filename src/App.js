e0fc2b3aa7831d87ef0386ba68d4e21fbe1a2b58
Dasha <dasha@example.com>
2021-09-12 13:34:35 +0400

    added basic template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.sass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        hvuvuvuv<br />
+        iiviivyiyvyii
       </header>
+      <aside className={styles.sidebar}>
+        fdgdfgdfgdfgdfgdfgdfgdfg
+      </aside>
+      <div className={styles.content}>
+        fgdfgdfgdfgd
+      </div>
     </div>
   );
 }
